854c4dda23b13ffcb3c2d82c8b478976631ab0d8
Quoc Nguyen Van <quoc.nguyen@ntq-solution.com.vn>
2022-12-09 00:16:25 +0700

    update search

--- crud/views.py
+++ crud/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Student
-from django.http import HttpResponse
 from .forms import StudentForm
+from django.core.paginator import Paginator
+from django.db.models import Q
 # Create your views here.
 
 
@@ -10,9 +11,21 @@ def homepage(request):
 
 
 def show_list_student(request):
+    search = request.GET.get('search', None)
+    order_by = request.GET.get('order_by', None)
+    page = request.GET.get('page', 1)
     student_list = Student.objects.all()
+    if search:
+        student_list = student_list.filter(Q(name__contains=search))
+    if order_by == "age":
+        student_list = student_list.order_by("age")
+    paginator = Paginator(student_list, 3)
+    page_object = paginator.page(page)
     return render(request, 'student.html', {
         "student_list": student_list,
+        "page_object": page_object,
+        "search": search,
+
     })
 
 
